e2c88dc68f856d4a9e5f6fa1c8b28fe558ae2064
Astemir Eleev <astemireleev@gmail.com>
2018-03-24 13:56:13 +0300

    Created Updatable protocol

---
+++ spritekit-water-node-ios-app/Water Node/Protocols/Updatable.swift
@@ -0,0 +1,13 @@
+//
+//  Updatable.swift
+//  spritekit-water-node-ios-app
+//
+//  Created by Astemir Eleev on 24/03/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+protocol Updatable {
+    func update(_ dt: CFTimeInterval)
+}
