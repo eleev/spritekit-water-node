7b5a98f2561cbcfcbb676d00dd39554d97d94182
Astemir Eleev <astemireleev@gmail.com>
2018-03-24 13:55:35 +0300

    Created Renderable protocol

---
+++ spritekit-water-node-ios-app/Water Node/Protocols/Renderable.swift
@@ -0,0 +1,13 @@
+//
+//  Renderable.swift
+//  spritekit-water-node-ios-app
+//
+//  Created by Astemir Eleev on 24/03/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+protocol Renderable {
+    func render()
+}
