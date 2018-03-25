88beadf75c9fbf7ac187b36c676fe55413369c4e
Astemir Eleev <astemireleev@gmail.com>
2018-03-25 10:44:03 +0300

    Implemented NumType+Random extension

---
+++ spritekit-water-node-ios-app/Extensions/NumTypes+Random.swift
@@ -0,0 +1,98 @@
+//
+//  NumTypes+Random.swift
+//  spritekit-water-node-ios-app
+//
+//  Created by Astemir Eleev on 25/03/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+// MARK: Int Extension
+
+public extension Int {
+    
+    /// Returns a random Int point number between 0 and Int.max.
+    public static var random: Int {
+        return Int.random(n: Int.max)
+    }
+    
+    /// Random integer between 0 and n-1.
+    ///
+    /// - Parameter n:  Interval max
+    /// - Returns:      Returns a random Int point number between 0 and n max
+    public static func random(n: Int) -> Int {
+        return Int(arc4random_uniform(UInt32(n)))
+    }
+    
+    ///  Random integer between min and max
+    ///
+    /// - Parameters:
+    ///   - min:    Interval minimun
+    ///   - max:    Interval max
+    /// - Returns:  Returns a random Int point number between 0 and n max
+    public static func random(min: Int, max: Int) -> Int {
+        return Int.random(n: max - min + 1) + min
+        
+    }
+}
+
+// MARK: Double Extension
+
+public extension Double {
+    
+    /// Returns a random floating point number between 0.0 and 1.0, inclusive.
+    public static var random: Double {
+        return Double(arc4random()) / 0xFFFFFFFF
+    }
+    
+    /// Random double between 0 and n-1.
+    ///
+    /// - Parameter n:  Interval max
+    /// - Returns:      Returns a random double point number between 0 and n max
+    public static func random(min: Double, max: Double) -> Double {
+        return Double.random * (max - min) + min
+    }
+}
+
+// MARK: Float Extension
+
+public extension Float {
+    
+    /// Returns a random floating point number between 0.0 and 1.0, inclusive.
+    public static var random: Float {
+        return Float(arc4random()) / 0xFFFFFFFF
+    }
+    
+    /// Random float between 0 and n-1.
+    ///
+    /// - Parameter n:  Interval max
+    /// - Returns:      Returns a random float point number between 0 and n max
+    public static func random(min: Float, max: Float) -> Float {
+        return Float.random * (max - min) + min
+    }
+}
+
+// MARK: CGFloat Extension
+
+public extension CGFloat {
+    
+    /// Randomly returns either 1.0 or -1.0.
+    public static var randomSign: CGFloat {
+        return (arc4random_uniform(2) == 0) ? 1.0 : -1.0
+    }
+    
+    /// Returns a random floating point number between 0.0 and 1.0, inclusive.
+    public static var random: CGFloat {
+        return CGFloat(Float.random)
+    }
+    
+    /// Random CGFloat between 0 and n-1.
+    ///
+    /// - Parameter n:  Interval max
+    /// - Returns:      Returns a random CGFloat point number between 0 and n max
+    public static func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return CGFloat.random * (max - min) + min
+    }
+}
