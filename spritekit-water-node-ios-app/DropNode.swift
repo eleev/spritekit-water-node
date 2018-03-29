7091d72d2e4e40c8c9cf8409bac18fae332b3fd8
Astemir Eleev <astemireleev@gmail.com>
2018-03-29 17:02:11 +0300

    Refactored Droplet class

--- spritekit-water-node-ios-app/DropNode.swift
+++ spritekit-water-node-ios-app/DropNode.swift
@@ -1,17 +1,18 @@
 //
-//  Droplet.swift
+//  DropNode.swift
 //  spritekit-water-node-ios-app
 //
-//  Created by Astemir Eleev on 24/03/2018.
+//  Created by Astemir Eleev on 28/03/2018.
 //  Copyright © 2018 Astemir Eleev. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import SpriteKit
 
-class Droplet: SKSpriteNode {
+class DropNode: SKSpriteNode {
     
     // MARK: - Properties
     
+    var isAboveWater: Bool = true
     var velocity: CGPoint = .zero
 }
