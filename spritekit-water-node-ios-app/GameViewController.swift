7c4695243606d6a3923dfcb1cb7101ae502c6a39
Astemir Eleev <astemireleev@gmail.com>
2018-03-30 19:11:47 +0300

    Added physics debug flag

--- spritekit-water-node-ios-app/GameViewController.swift
+++ spritekit-water-node-ios-app/GameViewController.swift
@@ -29,6 +29,8 @@ class GameViewController: UIViewController {
             
             view.showsFPS = true
             view.showsNodeCount = true
+            view.showsPhysics = true
+    
         }
     }
 
