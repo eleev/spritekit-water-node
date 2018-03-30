3744eaba60c62f907b5ff49bfb1129ecc49a4f46
Astemir Eleev <astemireleev@gmail.com>
2018-03-30 19:10:33 +0300

    Implemented CloudNode class

---
+++ spritekit-water-node-ios-app/CloudNode.swift
@@ -0,0 +1,23 @@
+//
+//  CloudNode.swift
+//  spritekit-water-node-ios-app
+//
+//  Created by Astemir Eleev on 30/03/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+class CloudNode: SKSpriteNode, Updatable {
+    
+    
+    // MARK: - Conformance to Updatable protocol
+    
+    func update(_ dt: CFTimeInterval) {
+        let width = size.width
+        let half = width / 2
+        if position.x + half > GameScene.viewportSize.width + width {
+            position.x = -width
+        }
+    }
+}
