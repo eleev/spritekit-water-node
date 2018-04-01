e22535fd5fbb89c2cff12c763398b99e1a08f43d
Astemir Eleev <astemireleev@gmail.com>
2018-04-01 18:00:35 +0300

    Updated project scheme

--- spritekit-water-node-ios-app/View Controllers/GameViewController.swift
+++ spritekit-water-node-ios-app/View Controllers/GameViewController.swift
@@ -26,7 +26,7 @@ class GameViewController: UIViewController {
             }
             
             view.ignoresSiblingOrder = true
-            
+        
             view.showsFPS = true
             view.showsNodeCount = true
             view.showsPhysics = true
